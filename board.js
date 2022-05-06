31f47b20ac2a75ea22b60972585f83c4e296317a
xtarzyk <xtarzyk@tutanota.com>
2022-05-06 13:45:25 +0200

    little improvents

--- board.js
+++ board.js
@@ -2,8 +2,8 @@ import { flipCard } from './main'
 
 const imgs = ['img/cat-black.jpg', 'img/cat-climbs.jpg', 'img/cat-flower.jpg', 'img/cat-grey.jpg', 'img/cat-grey-white.jpg', 'img/cat-licks.jpg', 'img/cat-sitting.jpg', 'img/cat-sneaky-black.jpg', 'img/cat-watch.jpg', 'img/cat-watch-2.jpg']
 
-$('<h1>Scores: </h1>').appendTo('.main__scores')
-$('<h1>0</h1>').addClass('result').appendTo('.main__scores')
+$('<h1>').text('Scores: ').appendTo('.main__scores')
+$('<h1>').text('0').addClass('result').appendTo('.main__scores')
 
 const shuffleArray = array => {
     for (let i = array.length - 1; i > 0; i--) {
@@ -18,10 +18,10 @@ const shuffleArray = array => {
 const createCells = () => {
     shuffleArray(imgs.concat(imgs))
         .forEach(img => {
-            const $cellFront = $('<div></div>').addClass('cell__front')
+            const $cellFront = $('<div>').addClass('cell__front')
             const $cellBack = $('<img>').addClass('cell__back').attr('src', img)
             
-            $('<div></div>')
+            $('<div>')
                 .addClass('board__cell')
                 .click(flipCard)
                 .append($cellFront, $cellBack)
